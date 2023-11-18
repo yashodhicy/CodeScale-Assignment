28a0122017b181343856e27f6aa893d4d0840e8c
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-11-18 09:36:17 +0200

    Show user details in profile

--- app/screens/Profile.tsx
+++ app/screens/Profile.tsx
@@ -1,17 +1,31 @@
 import { Text, View } from "@bacons/react-views";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "react-native";
 import { FIREBASE_AUTH } from "../../firebaseConfig";
 import { useNavigation } from "@react-navigation/native";
 
 const Profile = () => {
   const navigation = useNavigation();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const currentUser = FIREBASE_AUTH.currentUser;
+        setUser(currentUser);
+      } catch (error) {
+        console.error("Error fetching user data:", error.message);
+      }
+    };
+
+    fetchUserData();
+  }, []);
 
   const handleLogout = async () => {
     try {
       await FIREBASE_AUTH.signOut();
       // Navigate to the root page (Login)
-      navigation.navigate('Login')
+      navigation.navigate('Login');
     } catch (error) {
       console.error("Error signing out:", error.message);
     }
@@ -20,11 +34,15 @@ const Profile = () => {
   return (
     <View>
       <Text>Profile</Text>
+      {user && (
+        <>
+          <Text>Name: {user.displayName}</Text>
+          <Text>Email: {user.email}</Text>
+        </>
+      )}
       <Button title="Logout" onPress={handleLogout} />
     </View>
   );
 };
 
 export default Profile;
-
-
